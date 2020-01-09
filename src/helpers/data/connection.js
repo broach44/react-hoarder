b69b815fcedc5804e31d22fc24d116f3f0084942
Crystal Broach <crysland@gmail.com>
2020-01-08 23:42:15 -0600

    WIP

--- src/helpers/data/connection.js
+++ src/helpers/data/connection.js
@@ -2,9 +2,9 @@ import firebase from 'firebase/app';
 import firebaseKeys from '../apiKeys.json';
 
 const firebaseApp = () => {
-  if (!firebase.apps.length {
+  if (!firebase.apps.length) {
     firebase.initializeApp(firebaseKeys.firebaseKeys);
-  })
+  }
 };
 
 export default firebaseApp;
