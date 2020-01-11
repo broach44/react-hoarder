e1e2de9500befabc9e4f92dfcf42c620dd0760a3
Crystal Broach <crysland@gmail.com>
2020-01-11 10:49:05 -0600

    item list prints in mystuff and each item will print details when clicked

--- src/components/shared/SingleStuff/SingleStuff.js
+++ src/components/shared/SingleStuff/SingleStuff.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import stuffData from '../../../helpers/data/stuffData';
+
 import './SingleStuff.scss';
 
 class SingleStuff extends React.Component {
+  state = {
+    stuff: {},
+  }
+
+  componentDidMount() {
+    const { stuffId } = this.props.match.params;
+    stuffData.getSingleStuffById(stuffId)
+      .then((response) => {
+        this.setState({ stuff: response.data });
+      })
+      .catch((errFromGetSingleStuff) => console.error(errFromGetSingleStuff));
+  }
+
   render() {
+    const { stuff } = this.state;
     return (
       <div className="SingleStuff">
         <h1>Single Stuff</h1>
+        <div className="row justify-content-center">
+          <div class="card col-6">
+            <img src={stuff.itemImage} alt={stuff.itemName} class="card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">{stuff.itemName}</h5>
+              <p class="card-text">{stuff.itemDescription}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/helpers/data/stuffData.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getStuffByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/stuff.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((result) => {
+      const allStuffObj = result.data;
+      const stuffs = [];
+      if (allStuffObj != null) {
+        Object.keys(allStuffObj).forEach((stuffId) => {
+          const newStuff = allStuffObj[stuffId];
+          newStuff.id = stuffId;
+          stuffs.push(newStuff);
+        });
+      }
+      resolve(stuffs);
+    })
+    .catch((err) => reject(err));
+});
+
+const getSingleStuffById = (stuffId) => axios.get(`${baseUrl}/stuff/${stuffId}.json`);
+
+export default { getStuffByUid, getSingleStuffById };
