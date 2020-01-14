a88d45fcec5c8608c45d9fe582dcd1d1310820b5
Crystal Broach <crysland@gmail.com>
2020-01-13 18:09:11 -0600

    added a very small bit of styling

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './Home.scss';
 
 class Home extends React.Component {
@@ -7,6 +7,7 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <h1>Home</h1>
+        <Link className="btn btn-secondary" to="/stuff">Click to view My Stuff</Link>
       </div>
     );
   }
--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -19,7 +19,7 @@ class Navbar extends React.Component {
   render() {
     const { authed } = this.props;
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <Link className="navbar-brand" to="/">React Hoarder</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -28,7 +28,7 @@ class Navbar extends React.Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <Link className="nav-link" to="/home">Home <span className="sr-only">(current)</span></Link>
+              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/stuff/new">New</Link>
@@ -37,7 +37,7 @@ class Navbar extends React.Component {
               <Link className="nav-link" to="/stuff">My Stuff</Link>
             </li>
             </ul>
-    { (authed) && <button className="btn btn-outline-danger my-2 my-sm-0" onClick={this.logMeOut}>Logout</button>}
+    { (authed) && <button className="btn btn-outline-warning my-2 my-sm-0" onClick={this.logMeOut}>Logout</button>}
       </div>
     </nav>
     );
